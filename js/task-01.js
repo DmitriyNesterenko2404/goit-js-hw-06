324a2ca7e117eb7280fe72d502691b08453808fc
DmitriyNesterenko2404 <87898599+DmitriyNesterenko2404@users.noreply.github.com>
2021-11-30 14:50:24 +0200

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const categoryEl = document.querySelectorAll('li.item');
+console.log(`Number of categories: ${categoryEl.length}`);
+
+const elemUl = document.querySelector('.animal');
+console.log(`Category: ${elemUl.textContent}`);
+
+const listAnimalEl = document.querySelectorAll('.animals__item');
+console.log(`Elements: ${listAnimalEl.length}`);
